52fe11abb9765082ae7988fecca1e7c07fae6743
Peter <naughtnowwhen@gmail.com>
2018-09-11 18:35:53 -0700

    map function practice

---
+++ app.js
@@ -0,0 +1,7 @@
+//Todos
+
+//make a constructor for question 'chunks', maybe a this.message for alerting with a little priming information, a this.question for the question prompted to the user, and a this.correct for storing the correct answer, boolean, and a this.answer for storing the responder's answer.
+
+// a for loop to iterate through the question/answer object
+// for each question, the question will be prompted to the screen.
+
