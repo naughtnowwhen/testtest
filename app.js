4927ee0d3053a383f60395bda18fc75ad6305b90
Peter <naughtnowwhen@gmail.com>
2018-09-13 09:55:16 -0700

    further examples

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // of so first I wrote this add () (following from techsith example)
 // without the first if statement, that stops the function when n decrements to 0. but of course got an infinite loop because there was no exit condition. 
 
-// It's interesting because n is decrementing and increasing both.
+// Question (1) It's interesting because n is decrementing and increasing both.
 //the total is incrementing, the final return grows, but n is also decrementing as well, from the start to zero. It seems mysterious how this works, but it does. Recursion is tricky. 
 
 
@@ -17,4 +17,23 @@ let add = function (n){
   }
 };
 
-console.log(add(5));
+console.log(add(4));
+
+
+//techsith is wonderful! He explains it so well. Question(1) I had was clearly answered. internal to the first instance of the function n remains whatever value you pass, that doesn't change. But the second instance of add does keep changing and decrementing by -1 each time. So if you pass 3 it looks like this through the looping
+
+// 3 + 2 = 5
+// 3 + 2 + 1 = 6
+
+// and further here's something what it looks like internally:
+
+// add(3){
+//     return 3 + add (2) {
+//         return 2 + add(1){
+//             return 1 + add(0){
+//                 return 0;
+//             }
+//         }   
+//     }
+// }
+
